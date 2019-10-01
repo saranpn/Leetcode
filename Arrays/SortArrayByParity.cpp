973d64ceacc51bc35454223dbc02d4cebc5c8c01
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2019-09-30 21:22:07 -0700

    Update SortArrayByParity.cpp

--- Arrays/SortArrayByParity.cpp
+++ Arrays/SortArrayByParity.cpp
@@ -1,22 +1,21 @@
-/*Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+//Author: Saravana Kumar
+//
 
-You may return any answer array that satisfies this condition.
-
- 
+/*Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed
+by all the odd elements of A. You may return any answer array that satisfies this condition.
 
 Example 1:
-
 Input: [3,1,2,4]
 Output: [2,4,3,1]
 The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
- 
 
 Note:
-
 1 <= A.length <= 5000
 0 <= A[i] <= 5000
-
 */
+************************************************************************************************************
+//Solution 1:
+
 class Solution {
 public:
     
@@ -51,3 +50,25 @@ public:
         return A;
     }
 };
+
+*******************************************************************************************
+//Solution 2:
+
+class Solution {
+public:
+
+/* Two pointer i and j which points on odd and even and swap the elements when found */
+    vector<int> sortArrayByParity(vector<int>& A) {
+        int i=0;
+        for (int j = 0; j < A.size(); ++j) {
+            if (A[j] % 2 == 0) {
+                swap(A[i], A[j]);
+                i++;
+            }
+        }
+        return A;
+    }
+};
+
+*********************************************************************************************
+
