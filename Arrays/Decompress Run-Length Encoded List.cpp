cbc7b93ee778ba10e4136a1dfe393233c27669cb
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-01-20 20:10:38 -0800

    Update Decompress Run-Length Encoded List.cpp

--- Arrays/Decompress Run-Length Encoded List.cpp
+++ Arrays/Decompress Run-Length Encoded List.cpp
@@ -1,3 +1,7 @@
+
+// Source : https://leetcode.com/problems/decompress-run-length-encoded-list/
+// Author : Saravana Kumar
+
 /* We are given a list nums of integers representing a list compressed with run-length encoding.
 
 Consider each adjacent pair of elements [a, b] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are a elements with value b in the decompressed list.
