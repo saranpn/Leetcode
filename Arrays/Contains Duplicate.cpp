9c6ed8e638f8d5ff2a1b11d4a653dd846db74322
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-04-16 13:19:20 -0700

    Update Contains Duplicate.cpp

--- Arrays/Contains Duplicate.cpp
+++ Arrays/Contains Duplicate.cpp
@@ -23,28 +23,27 @@ public:
     bool containsDuplicate(vector<int>& nums) 
         
     {
-        
-    if(nums.size() <= 1) return 0;
     
+    //If there is only 1 number in the array
+    if(nums.size() <= 1) 
+    {
+        return 0;
+    }
+        
+    //Sorting the array
     sort(nums.begin(), nums.end());
     
+    //Comapring the adjacent elements
     for(int i = 0; i < nums.size() - 1; ++i)
     {
         if(nums[i] == nums[i+1])
         {   
-            cout << "True";
-            return 1;
-        }
-        else
-        {   
-            cout << "False";
-            return 0;
+
+            return 1;    //If true, return 1 and exit the function
         }
     }
-    
-    return 0; 
+            return 0;   //If false, return and exit the function 
 }
 };
-
 //************************************************  METHOD 2 - Using SETS ******************************
 
