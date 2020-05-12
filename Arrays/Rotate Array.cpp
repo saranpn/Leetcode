664cb2fe4a9a67ab896ad398b581672fd3649dc9
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-05-11 18:58:44 -0700

    Update Rotate Array.cpp

--- Arrays/Rotate Array.cpp
+++ Arrays/Rotate Array.cpp
@@ -1,16 +1,13 @@
 //Author: Saravana Kumar
 //Source: https://leetcode.com/problems/rotate-array/
 
-class Solution {
-    public:
-
-/* IDEA1: Using Extra(another) vector. Time complexity: O(n). Space complexity: O(n)
-
+/************************ IDEA1: Using Extra(another) vector. Time complexity: O(n). Space complexity: O(n) ***************************
 Store the last elements in first vector and store first elements in another vector. 
 Finally, push back the last elements of first vector inside the another vertor */
-      
+
+class Solution {
+    public: 
         void rotate(vector<int>& nums, int k)  //Taking the initial vector and k as parameter
-        
         { 
 //If that vector did not have any elemts in it (OR) the k value is less than or equal to zero. Just return and don't do anything
 
@@ -33,3 +30,30 @@ the same array with k=8. Why ?? For k % nums.size() ----> BOTH 2 % 6 and 8 % 6 w
                 nums.push_back(result[i]);
         }
     };
+
+
+
+/************************ IDEA2(BEST): Using Inplace Reverse. Time complexity: O(n). Space complexity: O(1) ***************************
+Step 1: Reverse the whole element.
+Step 2: Reverese the first elements with respect to k
+Step 3: Reverse the last elements with respect to k */
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) 
+    {
+        
+        if(nums.size() == 0 || k <= 0) return;
+        
+        k = k % nums.size();
+        
+        // Step 1: Reverse the whole element.
+        reverse(nums.begin(), nums.end());
+        // Step 2: Reverese the first elements with respect to k
+        reverse(nums.begin(), nums.begin() + k);
+        // Step 3: Reverse the last elements with respect to k
+        reverse(nums.begin()+k, nums.end());
+        
+    }
+};
+
