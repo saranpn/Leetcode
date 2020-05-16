a017b2df569b84f7fb93ce89390fc0a398d13755
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-05-15 20:19:05 -0700

    Update First Unique Character in a String.cpp

--- String/First Unique Character in a String.cpp
+++ String/First Unique Character in a String.cpp
@@ -13,12 +13,16 @@ return 2.
 Note: You may assume the string contain only lowercase letters.
 
 *****************************************************************************************************************************/
-/*  IDEA: Step 1: Traverse through the string once.
+/*  Solution1: Using Maps
+
+          Step 1: Traverse through the string once.
           Step 2: Store the key and its frequency.
           Step 3: Traverse through the string again 
           Step 4: Return the "value" which has only 1 frequency, the index
           Step 5: If there is no unique element return -1
 */
+
+// Time Complexity : (2*N) => O(N) (Since we are traversing twice) 
 
 class Solution {
 public:
@@ -38,5 +42,43 @@ public:
         }
         
         return -1; //Step 5
+    }
+};
+
+/***********************************************************************************************************************/
+
+// Solution 2: Using Vector by using ASCII method
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        vector<int> result(26,0);
+        
+        for(int i=0; i<s.size(); i++) //Traverse through thr string for each char
+        {
+            //Subtract each character with a (i.e. 97) --> we will get a number for each character
+            // e will be 5, c will be 3... etc. 
+            //This number will act as an index for the result vector we created
+            //temp is used to store index value for all the letters. 
+            int temp = s[i] - 'a';    
+            
+            //Increment that index value by 1 
+            //Increment 1 everytime whenever that index value appears
+            // (in our case 'e' index value which is 5 will appear thrice, so it is incremented 3 times)
+            result[temp]++;
+        }
+        
+        //Traverse though the result vector and find the first element whose value is 1 and return it.
+        for(int i=0; i<s.size(); i++) //We only need to traverse for the number of characters time in the result vector
+        {
+            if(result[temp] == 1)
+            {
+                return i;
+            }
+        }
+        
+        return -1; //If no unique element is found 
+        
     }
 };
