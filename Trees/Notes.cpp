38d8df3bd4fccbdef63176ef137b697757ed4190
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-06-01 22:36:03 -0700

    Create Notes.cpp

---
+++ Trees/Notes.cpp
@@ -0,0 +1,15 @@
+/* 
+
+------> Difference between Binary Tree and Binary Search Tree ??
+ANSWER: * Binary tree will have utmost 2 children for each node in the tree, it doesn't care about the value inside the tree.
+        * Binary Search Tree is a Binary tree, the value which are left side of the root will be less than the root value and the values 
+          which are on the right side of the root are greater than the root value.
+          
+------> Exit condition of Binary tree Traversal ??
+ANSWER: if(root == NULL) { return; }  (during recursive traversal)
+
+------> What happens when we do inorder traversal in the Binary Search Tree ? 
+ANSWER:  We get the value is sorted order
+          
+
+*/
