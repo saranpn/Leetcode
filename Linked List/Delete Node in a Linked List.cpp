ee8fb652a010395659825f81b992648fb55e119c
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-06-02 23:49:38 -0700

    Update Delete Node in a Linked List.cpp

--- Linked List/Delete Node in a Linked List.cpp
+++ Linked List/Delete Node in a Linked List.cpp
@@ -8,22 +8,19 @@ Input: head = [4,5,1,9], node = 5
 Output: [4,1,9]
 Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function */ 
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
- 
+/*  IDEA: Since we don't have head pointer, we follow this method. 
+          Instead of deleting the given node, delete the next node. 
+          But, make a copy of that next node, and delete that node. 
+*/
+
 class Solution {
 public:
     void deleteNode(ListNode* node) 
     {
-        ListNode *next = node->next;
-        node->val = next->val;
-        node->next = next->next;
-        delete next;
+        ListNode *temp; //create a node called temp in stack
+        temp = node->next; //make temp to point on given node's next
+        node->val = temp->val; //copy the value of the temp's node and store it in the node's node.
+        node->next = temp->next; //make node's next to point on temp's next
+        delete temp; //delete that temp node
     }
 };
