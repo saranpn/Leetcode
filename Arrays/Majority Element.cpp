9dc158a452a5380e314275a0c76c9f4f6b6fa1be
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-06-29 19:33:16 -0700

    Update Majority Element.cpp

--- Arrays/Majority Element.cpp
+++ Arrays/Majority Element.cpp
@@ -1,3 +1,6 @@
+
+//SOLUTION 1: Using Hash Map
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) 
@@ -13,5 +16,20 @@ public:
         }
         
         return -1; 
+    }
+};
+
+
+//Solution 2: Using Sorting 
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) 
+    {
+        //vector<int> nums = {1,1,2,2,5,5,5,5,2};
+        
+        sort(nums.begin(),nums.end());
+        
+        return nums[nums.size()/2]; //after sorting the majority element would be the center element in the array.
     }
 };
