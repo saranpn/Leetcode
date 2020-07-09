da210f2949a808f81a0477cbe6f9c5fb90f95100
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-07-08 18:20:35 -0700

    Create Search Insert Position.cpp

---
+++ Arrays/Search Insert Position.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    { 
+        int i = 0;
+      for(i; i<nums.size(); i++)
+      {
+          if(nums[i] >= target)
+          {
+              break;
+          }
+      }
+        
+        return i;
+    }
+};
