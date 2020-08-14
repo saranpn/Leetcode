c355010fa4d6cd9395afbfcf39a13cdf6e1a2ce6
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-08-13 17:38:46 -0700

    Create Product of Array Except Self.cpp

---
+++ Arrays/Product of Array Except Self.cpp
@@ -0,0 +1,45 @@
+/* Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? */
+
+/********************************************************************************************************************************************************/
+
+
+// Time: O(N) (Traversing the arrray twice) 
+// Space: O(N) (Creating an extra array for storing the result
+
+// This solution won't work if there is a zero in the array. 
+
+
+/* Idea: Multiply all the elements in the array and calculate total product value. Then traverse the array and divide each value by the current i value, 
+         to get the multiplication of all the other elements  */
+         
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        vector<int> result;
+        int totalproduct = 1;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            totalproduct = totalproduct * nums[i];
+        }
+        
+        for(int j=0; j<nums.size(); j++)
+        {
+            result.push_back(totalproduct/nums[j]);
+        }
+        
+        return result; 
+    }
+};
