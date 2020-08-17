1b0057bc50b32126cb903c8589b113355456f69b
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-08-16 19:35:44 -0700

    Update Kth Smallest Element in a Sorted Matrix.cpp

--- Heap/Kth Smallest Element in a Sorted Matrix.cpp
+++ Heap/Kth Smallest Element in a Sorted Matrix.cpp
@@ -1,2 +1,30 @@
 // Author: Saravana Kumar
-// Source: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+   
+    //creating a priority queue of matrix to implement max-heap
+    priority_queue<int,vector<int>> result;
+    
+ 
+    for(int i=0; i<matrix.size(); i++)
+    {
+        for(int j=0; j<matrix.size(); j++)
+        {
+            if(result.size()<k)
+            { //Traversing through the nums vector and push it into the priority_queue
+                result.push(matrix[i][j]);
+            }
+            //Afer Pushing, follow the same method 
+            else if(matrix[i][j] < result.top())
+            {
+                result.pop();
+                result.push(matrix[i][j]);
+            }
+        }
+    }
+    return result.top();
+    }
+};
