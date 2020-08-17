37b7153d26ce4a4a64a974e1980ec1dede1bcee9
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-08-16 18:31:32 -0700

    Update Kth Largest Element in an Array.cpp

--- Heap/Kth Largest Element in an Array.cpp
+++ Heap/Kth Largest Element in an Array.cpp
@@ -1,2 +1,51 @@
 // Author: Saravana Kumar
 // Source: https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+//***************************************************** SOLUTION 1: Using HEAP (Best) *******************************************************
+//Time: O(N log k)
+//Space: O(k)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        struct greaters
+        {  
+            bool operator()(const long& a,const long& b) const
+            {  return a>b;  }  
+        }; 
+        
+        vector<int> result(nums.begin(), nums.begin()+k);
+        make_heap(result.begin(), result.end(), greaters());
+        
+        for(int i=k; i<nums.size(); i++)
+        {
+            if(nums[i] > result.front())
+            {
+                //pop out that top number from min-heap
+                pop_heap(result.begin(),result.end(),greaters());
+                result.pop_back();
+
+                //Add or push that number from the nums vector to the min-heap
+                result.push_back(nums[i]);
+                push_heap(result.begin(), result.end(), greaters());
+            }
+        }
+        
+        return result.front();
+    }
+};
+
+
+//***************************************************** SOLUTION 2: Using Sorting Technique *******************************************************
+//Time: O(N log N)
+//Space: O(k) 
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        int ans = 0;
+        sort(nums.begin(),nums.end(), greater<int>());
+        return nums[k-1];
+    }
+};
