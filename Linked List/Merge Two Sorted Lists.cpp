db18460c314d799050e38a7ac04d28e26fa9327e
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-05 22:10:17 -0700

    Create Merge Two Sorted Lists.cpp

---
+++ Linked List/Merge Two Sorted Lists.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+    {
+        ListNode *dummy; //Creating a pointer which will get the final result
+        dummy = new ListNode;
+        //dummy->val = -1; 
+        ListNode* last = dummy;
+ 
+        while(l1 != NULL && l2 != NULL) {
+            if(l1->val < l2->val) {
+                last->next = l1;
+                last = l1;
+                l1 = l1->next;    //  time: O(N+M) space: O(1)
+            }
+            else {
+                last->next = l2;
+                last = l2;
+                l2 = l2->next;
+            }
+        }
+        
+        if(l1 != NULL) {
+            last->next = l1;
+        }
+        if(l2 != NULL) {
+            last->next = l2;
+        }
+        return dummy->next;
+    }
+};
