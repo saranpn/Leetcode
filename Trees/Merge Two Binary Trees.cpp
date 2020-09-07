7bcaa0688d36e5f807263a6207fb6a2fa862c2b6
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-07 14:36:12 -0700

    Create Merge Two Binary Trees.cpp

---
+++ Trees/Merge Two Binary Trees.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode *t1, TreeNode *t2) 
+    {
+        if(t1 == NULL) return t2; //Case 1 
+        if(t2 == NULL) return t1; //Case 2 
+        
+        //Case 3 (if we have atleast 1 node in both trees)
+        
+        t1->val = t1->val + t2->val; //Take the value from t2 and add it to the t1
+        
+        //Go to left subtrees of both the trees and call recursively, and then store the  that in t1-> left, which will be used for next call. 
+        t1->left = mergeTrees(t1->left,t2->left);
+        
+        //Similarly for right subtrees of both the trees
+        t1->right = mergeTrees(t1->right,t2->right);
+        
+        return t1;   
+    }
+};
