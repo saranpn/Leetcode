31c5649be619bdfe00321d92dd4cb5af19bb2a24
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-06 21:27:03 -0700

    Update Binary Tree Level Order Traversal (MEDIUM) .cpp

--- Trees/Binary Tree Level Order Traversal (MEDIUM) .cpp
+++ Trees/Binary Tree Level Order Traversal (MEDIUM) .cpp
@@ -1,3 +1,17 @@
+/* Level Order Traversal: Use Queue (BFS) 
+
+--> Create a Queue with node data type and push the root node to the Queue. 
+--> Inside the While loop (Until the Queue is not empty) 
+            => Make the pointer of node data type and to point the front node of the queue. 
+            => Pop out the element from Queue
+            //you can print the data in this line. 
+            => if there is a left child, then push it to the Queue
+            => if there is a right child, then push it to the Queue 
+            
+ ******************************************************************************************************************************/
+
+// Add a element to the Queue --> Then add it to the vector --> Then add it to the Matrix 
+
 class Solution {
 public:
     vector<vector<int>> result; //Matrix to store the result 
