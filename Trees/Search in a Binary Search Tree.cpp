31bb5d3e496f5d581e882cf93c058519bcb16172
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-09 23:25:53 -0700

    Update Search in a Binary Search Tree.cpp

--- Trees/Search in a Binary Search Tree.cpp
+++ Trees/Search in a Binary Search Tree.cpp
@@ -43,17 +43,15 @@ class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) 
     {
-        if(root == NULL)  //Return NULL if root is NULL
-            return NULL;
+        if(root==NULL) return {}; //Return NULL or empty set if root is NULL
         
-        if(val == root->val) //Returns the whole root with subtree(if any) if it finds the number ??
-            return root;      
+        if(root->val == val) return root; //Returns the whole root with subtree(if any) if it finds the number
         
-        else if(val < root->val)  //if the target value is less than the root value, 
-            return searchBST(root->left,val); //then move to left subtree and repeat the function
+        else if(root->val < val) //if the root value is less than target value,
+            return searchBST(root->right, val); //then move to right subtree and repeat the function 
         
-        else                      //if the target value is greater than the root value,
-            return searchBST(root->right,val);//then move to right subtree and repeat the function
+        else                                    //if the root value is greater than target value,
+            return searchBST(root->left, val);  //then move to left subtree and repeat the function 
     }
 };
  
