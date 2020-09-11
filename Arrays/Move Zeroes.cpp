6b3307f6420c09a61b186ed3e45313a64774e86d
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-10 18:56:21 -0700

    Update Move Zeroes.cpp

--- Arrays/Move Zeroes.cpp
+++ Arrays/Move Zeroes.cpp
@@ -1,16 +1,8 @@
-/* Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+/****************************************************** SOLUTION 1: In place solution ****************************************************************************
+//TIME: O(N) --> Traversing array once 
+//SPACE: O(N) --> Using Extra Array
 
-Example:
-
-Input: [0,1,0,3,12]
-Output: [1,3,12,0,0]
-Note:
-
-You must do this in-place without making a copy of the array.
-Minimize the total number of operations.
-
-*****************************************************************************************************/
-/* IDEA: By using Inplace method without using extra space (vector). So, we here use two pointers called left and right.
+ IDEA: By using Inplace method without using extra space (vector). So, we here use two pointers called left and right.
          At first both left and right pointer will be pointing on the zero index. 
          Right pointer used to see if it is zeor or non zero element.
          If it is zero, don't do anything just increment the right pointer.
@@ -41,3 +33,131 @@ public:
         }
     }
 };
+
+//FULL CODE: 
+#include <iostream>
+#include <vector> 
+#include <unordered_map> 
+
+using namespace std;
+
+vector<int> MoveZeros(vector<int> nums)
+{
+  int n = nums.size(); //size of the array 
+  int left=0;
+  int right=0;
+
+  for(int i=0; i<n; i++) //traverse the array
+  {
+    if(nums[right] == 0) {
+      right++;
+    }
+    
+    else if(nums[right] != 0) {
+      swap(nums[left],nums[right]);
+      left++;
+      right++;
+    }
+  }
+ 
+  //For printing out the result 
+ for(int i=0; i<n; i++)
+ {
+   cout << nums[i] << " ";
+ }
+  
+ return nums; 
+}
+
+int main() {
+    
+  vector<int> nums = {0,1,0,3,12}; 
+
+  MoveZeros(nums);
+  
+  return 0;
+}
+
+/****************************************************** SOLUTION 2:Using Extra Array ****************************************************************************/
+
+//TIME: O(N) --> Traversing array once 
+//SPACE: O(N) --> Using Extra Array
+
+/* IDEA: Traverse through the array once, and push the elements to the newly created array (only if is a non-zero element) 
+         Then, find the number zeros, that we need to push by, #of Zeros = (size of nums) - (size of result) 
+         And then push the zeros into the result vector */ 
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        vector <int> result; 
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] != 0)
+            {
+                result.push_back(nums[i]);
+            }
+        }
+        
+        int m = result.size();
+        
+        int count = n - m;
+        
+        for(int i=0; i<count; i++)
+        {
+            result.push_back(0);
+        }
+    }
+};
+
+//FULL CODE: 
+#include <iostream>
+#include <vector> 
+#include <unordered_map> 
+
+using namespace std;
+
+vector<int> MoveZeros(vector<int> nums, vector<int> result)
+{
+    int n = nums.size(); //size of the array 
+
+    for(int i=0; i<n; i++) //traverse the array
+  {
+      if(nums[i] != 0) 
+      {
+        //and push only the non-zero elements into the result vector 
+          result.push_back(nums[i]);
+      }
+  }
+  
+  int m = result.size(); //Get the size of the result array 
+  int count = n - m; //This will give us the number of 0's that we need to push back it to the result vector 
+
+  for(int i=0; i<count; i++) { //push the zeros 
+      result.push_back(0);
+  }
+  
+  int new_m = result.size(); //Get the new size of the result vector 
+ 
+  //For printing out the result 
+  for(int i=0; i<new_m; i++) {
+    cout << result[i] << " ";
+  }
+  
+  return result; //Finally return the result
+  
+}
+
+int main() {
+    
+  vector<int> nums = {0,1,0,3,12}; 
+  
+  vector <int> result; 
+  
+  MoveZeros(nums,result);
+  
+  return 0;
+}
