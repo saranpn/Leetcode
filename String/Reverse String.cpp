c039a8654e7c797701eda52d556f3a38a572eedd
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-11 08:13:11 -0700

    Update Reverse String.cpp

--- String/Reverse String.cpp
+++ String/Reverse String.cpp
@@ -1,25 +1,7 @@
-// Source : https://leetcode.com/problems/reverse-string/
-// Author : Saravana Kumar
-
-/***************************************************************************************************** 
-
-Write a function that reverses a string. The input string is given as an array of characters char[].
-
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-
-You may assume all the characters consist of printable ascii characters.
-
-Example 1:
-
-Input: ["h","e","l","l","o"]
-Output: ["o","l","l","e","h"]
-Example 2:
-
-Input: ["H","a","n","n","a","h"]
-Output: ["h","a","n","n","a","H"]
-
-******************************************************************************************************/
-//Solution 1:   // Time: O(n)  // Space: O(1)
+//************************************************** Solution 1: Using Pointers ********************************************************************************
+ 
+// Time: O(n)  
+// Space: O(1)
 
 class Solution {
 public:
@@ -32,7 +14,10 @@ public:
     }
 };
 
-//Solution 2:   // Time: O(n)  // Space: O(1)
+//************************************************** Solution 2: Using STL ********************************************************************************
+ 
+// Time: O(n)  
+// Space: O(1)
 
 class Solution{
 public:
@@ -41,3 +26,40 @@ public:
        reverse(s.begin(),s.end());
     }
 };
+
+//************************************************** Solution 3: Using Stack (not for Leetcode) ***********************************************************************
+ 
+// Time: O(n)  
+// Space: O(N)
+
+#include <iostream>
+#include <vector> 
+#include <stack>
+using namespace std;
+
+int main()
+{
+    vector<char> s = {'s','a','r','a','n'};
+    vector<char> result; 
+    stack<char> MyStack;
+    int n = s.size();
+    for(int i=0; i<n; i++)
+    {
+        MyStack.push(s[i]);
+    }
+    
+    while(!MyStack.empty())
+    {
+        result.push_back(MyStack.top());
+        MyStack.pop();
+    }
+    
+    int m = result.size();
+    
+    for(int j=0; j<m; j++)
+    {
+        cout << result[j] << " ";
+    }
+
+    return 0;
+}
