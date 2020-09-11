f16ebb1e985128f6e1316d8eaff0746b5b4ed218
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-10 22:37:36 -0700

    Update Maximum Subarray.cpp

--- Arrays/Maximum Subarray.cpp
+++ Arrays/Maximum Subarray.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        
-        int maxSum = nums[0];
-        int currSum = nums[0];
-        
-        for(int i=1; i<nums.size(); i++)
+    int maxSubArray(vector<int>& nums) 
+    {
+        int currMax = nums[0];
+        int maxSoFar = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) 
         {
-            currSum = max(nums[i] + currSum, nums[i]);
-            maxSum = max(maxSum,currSum);
-        }
+        /* Standing at a number we need to ask, 
+            1) By adding this number to the previous contiguous number will give us the maximum sum (OR)
+            2) By adding this number individually (which also means starting new contiguous sub-array) will give us the maximum sum   */ 
+            
+          int extend = currMax + nums[i]; //extend the contiguous subarray with previous numbers 
+          int start_new = nums[i]; //do we need to start the new contiguous sub_array by adding that number individually
+          currMax = max(extend, start_new); //then select the maximum number out of this two 
+            
+          /* Now we need to make sure that, this maximum number we obtained now is the maximum so far ?? 
+          
+              Then check, if that current maximum is the maximum number we obtained so far. 
+              If yes, then keep the current max number as maxSoFar 
+              If No, then keep the previous maxSoFar as new maxSoFar   */
         
-        return maxSum;
+          maxSoFar = max(maxSoFar, currMax);  
+        }
+
+        return maxSoFar; //Finally return the maxSoFar 
     }
 };
