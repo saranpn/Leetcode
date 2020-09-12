5bc0c859f37bd1f78bd480c3f6a082cf54d9a535
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-11 18:36:15 -0700

    Update Middle of the Linked List.cpp

--- Linked List/Middle of the Linked List.cpp
+++ Linked List/Middle of the Linked List.cpp
@@ -1,3 +1,8 @@
+
+//************************************* Solution 1: Using Counter ********************************************************
+//Time: ON) (Where we are traversing the list almost twice) 
+//Space: O(1) (Constant space) 
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) 
@@ -21,6 +26,37 @@ public:
         }
         
         return slow;
+    }
+};
+
+
+
+//************************************* Solution 2: Using Counter ********************************************************
+//Time: ON) (Where we are traversing the list almost twice) 
+//Space: O(1) (Constant space) 
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) 
+    {
+        int count = 0;  //initialize counter 
+        
+        ListNode* temp = head; //create a pointer 
+        
+        while(temp!=NULL) 
+        {
+            count++; //make a count of number of nodes 
+            temp = temp->next; 
+        }
+        
+        temp = head; //Again make the temp pointer to point on head 
+        
+        for(int i=0; i<count/2; i++) //Then travel upto the 
+        {
+            temp = temp->next;
+        }
+        
+        return temp; //Finally return the temp node 
         
     }
 };
