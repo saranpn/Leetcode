3d07bf622beac5e5eed5589f6d53e9435678e586
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-13 16:02:19 -0700

    Update Set Matrix Zeroes.cpp

--- Matrix/Set Matrix Zeroes.cpp
+++ Matrix/Set Matrix Zeroes.cpp
@@ -7,25 +7,26 @@ Output: [[1,0,1],[0,0,0],[1,0,1]  */
 
 /* SOLUTION 1: BRUTE FORCE METHOD   (NOT OPTIMAL)
 
-              Step 1: Create a two separate vector, 1 vector with row size of the matrix and other vector with column size of the matrix. 
-              Step 2: Traverse through the matrix, and if you find a zero element, then make the corresponding row vector and column vector to be true. 
-                                    Example: row_zero = [0 1 0];
-                                             column_zero = [0 1 0];
-              Step 3: Then traverse through the matrix again, and if find row_zero vector to be true or col_zero vector to be true, then replace that element in the  
-                      matrix to be zero. 
+Step 1: Create a two separate vector, 1 vector with row size of the matrix and other vector with column size of the matrix. 
+Step 2: Traverse through the matrix, and if you find a zero element, then make the corresponding row vector and column vector to be true. 
+                        Example: row_zero = [0 1 0];
+                             column_zero = [0 1 0];
+Step 3: Then traverse through the matrix again, and if find row_zero vector to be true or col_zero vector to be true, then replace that 
+element in the matrix to be zero. 
 
                       
-                                    //TIME COMPLEXITY: 
-                                    //SPACE COMPLEXITY: O(M + N)       */
+//TIME COMPLEXITY: 
+//SPACE COMPLEXITY: O(M + N)       */
 
 
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
     int h = matrix.size();
     int w = matrix[0].size();
     
-    //vector<bool> row_zero(h);
+    vector<bool> row_zero(h);
     vector<bool> column_zero(w);
     
     for(int i=0; i<h; i++)
@@ -34,6 +35,7 @@ public:
         {
             if(matrix[i][j] == 0)
             {
+                row_zero[i] = true;
                 column_zero[j] = true;
             }
         }
@@ -43,7 +45,55 @@ public:
     {
         for(int j=0; j<w; j++)
         {
+           if(row_zero[i] == true || column_zero[j] ==true)
+           {
+               matrix[i][j] = 0;
+           }
+        }
+    }
+    
+    for(int i=0; i<h; i++)
+    {
+        for(int j=0; j<w; j++)
+        {
            if(column_zero[j] ==true)
+           {
+               matrix[i][j] = 0;
+           }
+        }
+    }
+    }
+};
+
+
+// ************************************************************ FULL CODE: *************************************************************** 
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
+    int h = matrix.size();
+    int w = matrix[0].size();
+    
+    vector<bool> row_zero(h);
+    vector<bool> column_zero(w);
+    
+    for(int i=0; i<h; i++)
+    {
+        for(int j=0; j<w; j++)
+        {
+            if(matrix[i][j] == 0)
+            {
+                row_zero[i] = true;
+                column_zero[j] = true;
+            }
+        }
+    }
+        
+    for(int i=0; i<h; i++)
+    {
+        for(int j=0; j<w; j++)
+        {
+           if(row_zero[i] == true || column_zero[j] ==true)
            {
                matrix[i][j] = 0;
            }
@@ -63,55 +113,3 @@ public:
         
     }
 };
-
-
-// *************************************************************************** FULL CODE: ************************************************************************* 
-#include <vector> 
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    vector<vector<int>> matrix = {{1,1,1},{1,0,1},{1,1,1}};
-    
-    int h = matrix.size();
-    int w = matrix[0].size();
-    
-    vector<bool> row_zero(h);
-    vector<bool> column_zero(w);
-    
-    for(int i=0; i<h; i++)
-    {
-        for(int j=0; j<w; j++)
-        {
-            if(matrix[i][j] == 0)
-            {
-                row_zero[i] = true;
-                column_zero[j] = true;
-            }
-        }
-    }
-    
-    for(int i=0; i<h; i++)
-    {
-        for(int j=0; j<w; j++)
-        {
-           if(row_zero[i] == true || column_zero[j] ==true)
-           {
-               matrix[i][j] = 0;
-           }
-        }
-    }
-    
-    for(int i=0; i<h; i++)
-    {
-        for(int j=0; j<w; j++)
-        {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-    
-    return 0;
-}
