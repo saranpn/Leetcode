847ba3e38d342966d62b5bae0071433294f9174f
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-12 19:40:37 -0700

    Update Search in a Binary Search Tree.cpp

--- Trees/BST/Search in a Binary Search Tree.cpp
+++ Trees/BST/Search in a Binary Search Tree.cpp
@@ -1,43 +1,6 @@
-//Author: Saravana Kumar 
-//Source: https://leetcode.com/problems/search-in-a-binary-search-tree/discuss/?currentPage=1&orderBy=most_votes&query=c%2B%2B
-
-/********************************************************************************************************************************
-
-Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
-
-For example, 
-
-Given the tree:
-        4
-       / \
-      2   7
-     / \
-    1   3
-
-And the value to search: 2
-You should return this subtree:
-
-      2     
-     / \   
-    1   3
-    
- *********************************************************************************************************************************/
- 
- /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- 
-*/
-
-// Here, val ---> is the value we want to search
-//       root->val ---> is the data value of the particular root node 
-
-/*************************************** RECURSIVE METHOD ***************************************************/
+//*************************************** SOLUTION 1: RECURSIVE METHOD ***************************************************
+// TIME: O(N)
+// SPACE: O(1) 
 
 class Solution {
 public:
