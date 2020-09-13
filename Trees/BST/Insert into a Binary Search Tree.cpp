097f043207a036d3cec01b82aa8bc3b3a2f60be2
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-12 22:56:04 -0700

    Update and rename Trees/Insert into a Binary Search Tree.cpp to Trees/BST/Insert into a Binary Search Tree.cpp

--- Trees/BST/Insert into a Binary Search Tree.cpp
+++ Trees/BST/Insert into a Binary Search Tree.cpp
@@ -28,16 +28,20 @@ class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) 
     {
-        if(root == NULL) return new TreeNode(val);
-        
-        if(root->val > val) {
-            root->left = insertIntoBST(root->left, val);
+        if(root == NULL) //insertion of new node with value takes place here 
+        {
+            TreeNode* temp = new TreeNode(val);
+            root = temp; 
+            return root;
         }
+        
+        if(val < root->val) 
+            root->left = insertIntoBST(root->left,val);
+        
+        else if(val > root->val) 
+            root->right = insertIntoBST(root->right,val);
             
-        else {
-            root->right = insertIntoBST(root->right, val);
-        }
-        
-        return root;
+        return root; 
     }
+    
 };
