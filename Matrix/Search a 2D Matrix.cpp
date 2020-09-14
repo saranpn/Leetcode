e23bfda7373382e3991bf61cbfa4e58c2654a07b
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-13 17:27:39 -0700

    Update Search a 2D Matrix.cpp

--- Matrix/Search a 2D Matrix.cpp
+++ Matrix/Search a 2D Matrix.cpp
@@ -1,40 +1,15 @@
 /*  LINK: https://www.youtube.com/watch?v=eT0UqrYuqbg&t=133s
 
-QUESTION: 
-
-Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
-
-Integers in each row are sorted from left to right.
-The first integer of each row is greater than the last integer of the previous row.
-Example 1:
-
-Input:
-matrix = [
-  [1,   3,  5,  7],
-  [10, 11, 16, 20],
-  [23, 30, 34, 50]
-]
-target = 3
-Output: true
-
-****************************************************************************************************************************************/
-
 /* IDEA: // *************************************** Only applicable for "SORTED MATRIX" **************************************************
 
-//Consider the matrix as an array and do the following: 
---> Take two pointer "row" and "column"
---> With that caluclate the "left" pointer and "right" pointer of the array
-After this, will come inside the while loop. 
---> Then, with the help of left and right pointer, calculate the middle pointer of the array. 
-
-//Consider the matrix as Matrix iteslf and do the folowing: 
---> with the help of "middle" pointer and "column" calculate the "row_matrix" value of the matrix.
---> with the help of "middle" pointer and "column" calculate the "col_matrix" value of the matrix.
-
-//Then,
---> if the target == matrix[row_matrix][col_matrix], then return true (element found)
---> if the target < matrix[row_matrix][col_matrix], then cut the matrix into half by reducing the right pointer position
--->  if the target > matrix[row_matrix][col_matrix], then cut the matrix into half by increasing the left pointer position 
+--> Since, the Matrix is sorted. Divide the matrix into 2 halves. 
+           And traverse through only 1 half to find the target. 
+ 
+--> Step 1: Find the middle element of the matrix, by keeping a pointer at 1st and last element of the matrix. 
+--> Step 2: Find if the target is less or greater than the middle element.
+            If less, move the last pointer to the middle, and search only in the 1st half. 
+            If more, move the start pointer to the middle, and search only in the 2nd half. 
+--> Step 3: Continue this untill, we find the target (OR) the start pointer equals to end.
 
 **************************************************************************************************************************************************/
 
@@ -42,36 +17,34 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) 
     {
-    if(matrix.size() == 0) { return false;}
+        if(matrix.size() == NULL) return false; //Base case 
+      
+        int row = matrix.size(); //will give you the row size 
+        int column = matrix[0].size(); //will give you the column size 
         
-    int row = matrix.size();
-    int column = matrix[0].size();
-    
-    int left = 0;
-    int right = row * column -1; 
-    
-    
-    while(left<=right)
-    {
-        int middle = left + (right-left)/2; //will give you the middle index of the ARRAY
+        int start = 0; //matrix element count starts from 0 
+        int end = row * column -1;  //last number count of the matrix 
         
-        int row_matrix = middle/column; //[middle/column] --> Will give the ROW index of the middle element for MATRIX
-        int col_matrix = middle%column; //[middle%column] --> Will give the COLUMN index of the middle element for MATRIX 
+        while(start <= end) 
+        {
+            //will give you the middle index of the ARRAY at 1st iteration 
+            int middle = start + (end-start) /2; 
         
-        if(target == matrix[row_matrix][col_matrix])
-        {
-            return true;
-        }
-        else if(target < matrix[row_matrix][col_matrix])
-        {
-            right = middle - 1;
-        }
-        else if(target > matrix[row_matrix][col_matrix])
-        {
-            left = middle + 1;
-        }
-    }
+            int row_matrix = middle/column; //Will give the ROW index of the middle element for MATRIX @ 1st iteration
+            int col_matrix = middle%column; //Will give the COLUMN index of the middle element for MATRIX @ 1st iteration
 
-    return false;
+            //if the element is found return true 
+            if(target == matrix[row_matrix][col_matrix]) 
+                return true;
+            
+            //if the target is less than the middle element, then search only in the 1st half of the matrix 
+            else if(target < matrix[row_matrix][col_matrix]) 
+                end = middle-1;
+            
+            //if the target is greater than the middle element, then search only in the 2nd half of the matrix 
+            else if(target > matrix[row_matrix][col_matrix]) 
+                start = middle+1; 
+        }
+        return false; //if not found then, return false 
     }
 };
