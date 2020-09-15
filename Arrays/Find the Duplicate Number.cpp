6984e667f9540898702a401ca81f407307b2425a
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-14 21:51:31 -0700

    Update Find the Duplicate Number.cpp

--- Arrays/Find the Duplicate Number.cpp
+++ Arrays/Find the Duplicate Number.cpp
@@ -1,3 +1,7 @@
+//*************************************************** SOLUTION 1: Using MAP **************************************************************
+//Time: O(N)
+//Space: O(N)
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) 
@@ -20,5 +24,66 @@ public:
         }
         
         return 0;
+    }
+};
+
+//*************************************************** SOLUTION 2: Using Set **************************************************************
+//Time: O(N)
+//Space: O(N)
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int ans = 0; 
+        
+        set<int> MySet;
+        
+        for(int i=0; i<n; i++)
+        {
+            if(MySet.count(nums[i]))
+            {
+                ans = nums[i];
+            }
+            
+            MySet.insert(nums[i]);
+        }
+        
+        return ans; 
+    }
+};
+
+//*************************************************** SOLUTION 3: Using Sorting Technique ************************************************
+//Time: O(N log N)
+//Space: O(1)
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+        
+        int n = nums.size();
+        int ans = 0;
+        
+        int left = 0;
+        int right = 1;
+        
+        while(right<n)
+        {
+            if(nums[left] != nums[right])
+            {
+                left++;
+                right++;
+            }
+            else if(nums[left] == nums[right])
+            {
+                ans = nums[left];
+                break;
+            }
+        }
+        
+        return ans; 
     }
 };
