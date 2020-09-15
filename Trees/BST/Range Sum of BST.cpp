4e39c80c5257c5a5ba004bee90c498ad1faa7c00
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-15 11:09:55 -0700

    Update Range Sum of BST.cpp

--- Trees/BST/Range Sum of BST.cpp
+++ Trees/BST/Range Sum of BST.cpp
@@ -9,15 +9,15 @@ public:
         if(root==NULL) return 0;
         
         int sum = 0; //initializing sum
-        sum_function(root,L,R,&sum); //calling the sum_function 
+        sum_function(root,L,R,sum); //calling the sum_function 
         return sum; //Finally return the sum 
     }
         
-    void sum_function(TreeNode* root, int L, int R, int *sum)
+    void sum_function(TreeNode* root, int L, int R, int &sum)
     {
         if(root->val >=L && root->val <=R) //if root value is between L and R, add root value to sum
         {
-            *sum = *sum + root->val; 
+            sum = sum + root->val; 
         }
 
         //Keep calling the left and right child until if it is not null 
