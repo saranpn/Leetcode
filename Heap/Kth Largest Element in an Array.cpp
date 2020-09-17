185054e715cfd0a67a5e6ea7915041afbd2c9b0d
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-16 18:34:33 -0700

    Update Kth Largest Element in an Array.cpp

--- Heap/Kth Largest Element in an Array.cpp
+++ Heap/Kth Largest Element in an Array.cpp
@@ -117,3 +117,45 @@ public:
         return nums[k-1];
     }
 };
+
+//********************************************************  FULL CODE: MIN HEAP *************************************************************************************
+#include <iostream>
+#include <vector> 
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int KthLargestNumber(vector<int> nums, int k, int result)
+{
+    priority_queue<int,vector<int>,greater<int>> Q;
+    
+    int n = nums.size();
+    
+    for(int i=0; i<k; i++)
+    {
+        Q.push(nums[i]);
+    }
+    
+    for(int i=k; i<n; i++)
+    {
+        if(Q.top()<nums[i])
+        {
+            Q.pop();
+            Q.push(nums[i]);
+        }
+    }
+    
+    return Q.top();
+}
+
+int main()
+{
+    vector<int> nums = {10,50,40,30,20};
+    int k = 3;
+    int result = 0;
+    result = KthLargestNumber(nums,k, result);
+    cout << result;
+    return 0;
+}
+
