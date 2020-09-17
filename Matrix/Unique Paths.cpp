4875a0cc97cdb91edc28704a64679e8cba86955f
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-09-16 19:44:59 -0700

    Update Unique Paths.cpp

--- Matrix/Unique Paths.cpp
+++ Matrix/Unique Paths.cpp
@@ -31,3 +31,50 @@ public:
         
     }
 };
+
+//************************************************************************* FULL CODE ******************************************************************************
+#include <iostream>
+#include <vector> 
+#include <algorithm>
+
+using namespace std;
+
+int uniquePaths(vector<vector<int>> matrix)
+{
+    int row = matrix.size();
+    cout << "m = " << row << endl;
+    int col = matrix[0].size();
+    cout << "n = " << col << endl;
+    
+    for(int i=0; i<col; i++) 
+    {
+        matrix[row-1][i] = 1; //fix row-1 and increment column from 0 to last column
+    }
+    
+    for(int i=0; i<row; i++)
+    {
+        matrix[i][col-1] = 1; //fix col-1 and increment column from 0 to last row
+    }
+    
+    for(int i=row-2; i>=0; i--) //start from row-2 and decrement till 0
+    {
+        for(int j=col-2; j>=0; j--) //start from col-2 and decrement till 0
+        {
+            //Add the two possible path that can be taken from the [i][j] index,
+            //We can go down, which is [i+1][j]
+            //We can go right, which is [i][j+1]
+            matrix[i][j] = matrix[i+1][j] + matrix[i][j+1]; 
+        }
+    }
+    
+    cout << matrix[0][0];
+}
+
+int main()
+{
+    //vector<vector<int>> matrix = {{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0}};
+    vector<vector<int>> matrix(7,vector<int>(3));
+    uniquePaths(matrix);
+    
+    return 0;
+}
