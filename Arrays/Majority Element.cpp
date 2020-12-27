9adea24058a1a0cb4b030c38805607240de65d0d
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-12-27 13:16:45 -0800

    Update Majority Element.cpp

--- Arrays/Majority Element.cpp
+++ Arrays/Majority Element.cpp
@@ -1,5 +1,38 @@
+//*********************************************************** SOLUTION 1: Using Counter ********************************************************************************
+//Time Complexity: O(N) 
+//Space Complexity: O(1) 
 
-//*********************************************************** SOLUTION 1: Using Hash Map ********************************************************************************
+/* IDEA: Since the majority element is more than N/2 times, even if we cancel all the other element with the majority element, 
+                we would still be left with 1 element which would be the majority element */
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) 
+    {
+        int n = nums.size();
+        
+        int majority = NULL;
+        int count = 0; 
+        
+        for(int i=0; i<n; i++)
+        {
+            //whenenver count is 0, assign new majority element
+                //For first iteration, first element would be the majority element
+                //For other iteration, count will be zero, means all the other elements got cancelled out, so we need to assign new majority element 
+            if (count == 0) majority = nums[i]; 
+            
+            if(nums[i] == majority) //if element equals to majority element,
+                count = count + 1;  //then increment count 
+            else if(nums[i] != majority) //if element is not equals to majority element
+                count = count - 1;  //then decrement count 
+        }
+        
+        return majority; //Finally return the majority element 
+    }
+};
+
+
+//*********************************************************** SOLUTION 2: Using Hash Map ********************************************************************************
 //Time Complexity: O(N) 
 //Space Complexity: O(N) 
 
@@ -7,17 +40,18 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) 
     {
-        unordered_map<int,int> MyMap;
+        unordered_map<int,int> MyMap; 
         
-        for(int i=0; i<nums.size(); i++)
+        int ans = 0; 
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++)
         {
-            MyMap[nums[i]] ++;
+            MyMap[nums[i]]++;
             
-            if(MyMap[nums[i]] > nums.size()/2 )
-                return nums[i];
+            if(MyMap[nums[i]] > n/2) ans = nums[i];
         }
-        
-        return -1; 
+        return ans; 
     }
 };
 
@@ -62,7 +96,7 @@ int main() {
 }
 
 
-//****************************************************************  Solution 2: Using Sorting  *************************************************************************** 
+//****************************************************************  Solution 3: Using Sorting  *************************************************************************** 
 
 //Time Complexity: O(N log N) --> Because for sorting, time complexity is always O(N log N), where N is the number of elements we are sorting 
 //Space Complexity: O(1) --> We are not using any extra space here 
