86385887ab11c5fb11e6881a5cfced8f81e9e13e
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-12-27 18:27:43 -0800

    Update Find All Numbers Disappeared in an Array.cpp

--- Arrays/Find All Numbers Disappeared in an Array.cpp
+++ Arrays/Find All Numbers Disappeared in an Array.cpp
@@ -1,3 +1,35 @@
+//Refer link: https://www.youtube.com/watch?v=CTBEcmzLAuA&ab_channel=TerribleWhiteboard
+
+/********************************************************** SOLUTION 1: Using No Extra Space ******************************************
+//Time: O(N)
+//Space: O(1)
+
+/* IDEA: Traverse the array and mark the element -ve by considering it as an index number. 
+         Then traverse the array again, and push the positive elements into the result vector, which would be the answer. 
+*/
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int> result;
+        
+        for(int i=0; i<n; i++) //Traverse the array 
+        {
+            int index = abs(nums[i]) - 1; // -1 is to consider all the elements as index, because index starts from zero. 
+            nums[index] = abs(nums[index]) * -1; // And abs is to consider only +ve numbers, since we already changed that to -ve
+        }
+        
+        for(int i=0; i<n; i++) //Traverse the array 
+        {
+            if(nums[i] > 0) //If a number is +ve 
+                result.push_back(i+1); //Then, push i+1, which would be the missing number 
+        }
+        
+        return result; 
+    }
+};
 /********************************************************** SOLUTION 2: Using Hash Set ***************************************************
 //Time: O(N)
 //Space: O(N)
