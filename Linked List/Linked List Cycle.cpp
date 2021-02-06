93c361d846207e259520a3c2c4e71c6988825757
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2021-02-05 19:12:56 -0800

    Update Linked List Cycle.cpp

--- Linked List/Linked List Cycle.cpp
+++ Linked List/Linked List Cycle.cpp
@@ -1,5 +1,9 @@
 //Link: https://www.youtube.com/watch?v=5cbPl9D-9SE&ab_channel=AmellPeralta
 
+//*************************************************************** SOLUTION 1: Using Slow and Fast Pointer **************************************************
+//Time: O(N)
+//Space: O(1)
+
 //TWO POINTER Approach: Slow and fast pointer. If both becomes equal at one point, then there is a loop. 
 
 class Solution {
@@ -25,5 +29,31 @@ public:
         }
         
         return false; //return false, if there is no loop 
+    }
+};
+
+//*************************************************************** SOLUTION 2: Using Set Or Map **************************************************
+//Time: O(N)
+//Space: O(N)
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) 
+    {
+        if(head == NULL) return false;
+        
+        set<ListNode*> MySet;
+        
+        ListNode* temp = head;
+        
+        while(temp != NULL && temp->next != NULL)
+        {
+            if(MySet.count(temp)) return true; 
+
+            MySet.insert(temp);
+            temp = temp->next;
+        }
+        
+        return false; 
     }
 };
